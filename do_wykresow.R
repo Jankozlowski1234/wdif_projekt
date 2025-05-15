739e3d1514c726a114e8e9c0a824c90dbc895cc7
Jankozlowski1234 <jaa.kozaa@gmail.com>
2025-05-15 14:42:00 +0200

    wykresy
    dodanie pierszych wykresów

--- do_wykresow.R
+++ do_wykresow.R
@@ -1,2 +1,10 @@
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(ggplot2)
+library(dplyr)
 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+dane_rozne_S_0<-read.csv("./dane/dane_odwr_t_2_sigma_0.3_S_0_od_45.0_do_54.5_r_0.02_K_48_T_2_.csv",
+                         header = T,sep = ",")
+
+
+subset(dane_rozne_S_0,dane_rozne_S_0$wersja=="put") %>%
+  ggplot(aes(x=  S_0,y = cena_opcji,col = opcja))+geom_line()
